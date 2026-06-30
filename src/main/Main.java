68740553a332102ac908468cbb6e2c4f5174719d
ReinaldoAJ7 <142172046+ReinaldoAJ7@users.noreply.github.com>
2026-06-30 20:06:51 -0300

    First class: Position

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,10 @@
 package main;
 
+import boardgame.Position;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World!!");
+		Position pos = new Position(7, 2);
+		System.out.println(pos);
 	}
 }
