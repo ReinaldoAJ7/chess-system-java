ddb8295cf3b7be74b43c73f50936d4ca90018f6e
ReinaldoAJ7 <142172046+ReinaldoAJ7@users.noreply.github.com>
2026-06-30 20:32:07 -0300

    Starting to implement Board and Piece

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,9 @@
 package main;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class Main {
 	public static void main(String[] args) {
-		Position pos = new Position(7, 2);
-		System.out.println(pos);
+		Board board = new Board(7, 7);
 	}
 }
