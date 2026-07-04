9cf153f49109c81ff466ab13a5fc4abab296f5f2
ReinaldoAJ7 <142172046+ReinaldoAJ7@users.noreply.github.com>
2026-07-03 23:18:03 -0300

    Possible moves of a piece

--- src/main/UI.java
+++ src/main/UI.java
@@ -42,7 +42,7 @@ public class UI {
 			int row = Integer.parseInt(s.substring(1));
 			return new ChessPosition(column, row);
 		} catch (RuntimeException e) {
-			throw new InputMismatchException("Erro reading ChessPosition.\n" + "Valid valeus are from a1 to h8.");
+			throw new InputMismatchException("Erro reading ChessPosition.\n" + "Valid values are from a1 to h8.");
 		}
 	}
 
